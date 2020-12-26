c55076d44fc83f72e4a1e8a6da0fe94c5477c2bb
Melissa Lee <plhw90@gmail.com>
2020-12-26 15:10:04 -0700

    weather.js is modified.

--- weather.js
+++ weather.js
@@ -1,4 +1,22 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "8d2922d0aadc1d2859019f748310376a";
 const COORDS = 'coords';
+
+function getWeather(lat, lon){
+    fetch(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+        )
+        .then(function(response){
+            return response.json()
+        })
+        .then(function(json){
+            // console.log(json)
+            const temperature = json.main.temp;
+            const place = json.name;
+            weather.innerText = `${temperature} @ ${place}`;
+        })
+}
 
 function saveCoords(coordsObj){
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +30,7 @@ function handleGeoSuccess(position){
         longitude
     };
     saveCoords(coordsObj);
+    getWeather(latitude, longitude)
 }
 
 function handleGeoError(){
@@ -28,6 +47,9 @@ function loadCoords(){
         askForCoords();
     } else {
         // getWeather
+        const parsedCoords = JSON.parse(loadedCoords);
+        console.log(parsedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
 
