d7951d924c283a53e31f211c157916a389c5214e
Melissa Lee <plhw90@gmail.com>
2020-12-25 23:35:30 -0700

    todo.js created.

--- greeting.js
+++ greeting.js
@@ -5,6 +5,23 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
       SHOWING_CN = "showing";
 
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    //console.log(currentValue);
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
@@ -15,6 +32,7 @@ function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null) {
         // she is not
+        askForName();
     } else {
         // she is
         paintGreeting(currentUser);
---
+++ todo.js
@@ -0,0 +1,58 @@
+const toDoForm = document.querySelector(".js-toDoForm"),
+      toDoInput = toDoForm.querySelector("input"),
+      toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS = "toDos";
+
+const toDos = [];
+
+function saveToDos(){
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text){
+    // console.log(text);
+    const li = document.createElement("li");
+    const delBtn = document.createElement("button");
+    const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerText = "❌"; 
+    span.innerText = text;
+    li.appendChild(delBtn);
+    li.appendChild(span);
+    li.id = newId;
+    toDoList.appendChild(li);
+    const toDoObj = {
+        text: text,
+        id: newId
+    };
+    toDos.push(toDoObj);
+    saveToDos();
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = toDoInput.value;
+    paintToDo(currentValue);
+    toDoInput.value = "";
+}
+
+function loadToDos(){
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null) {
+        // console.log(loadedToDos);
+        const parsedToDos = JSON.parse(loadedToDos);
+        // console.log(parsedToDos);
+        parsedToDos.forEach(function(toDo){
+            // console.log(toDo.text);
+            paintGreeting(toDo.text);
+        })
+    }
+}
+
+function init() {
+    loadToDos();
+    toDoForm.addEventListener("submit", handleSubmit);
+}
+
+init();
